9c3aea76980b7086ef1a4d4f9f06bdb0538158e1
Jorge Casariego <jorgekasa@gmail.com>
2015-03-20 00:12:51 -0300

    First Commit

---
+++ TaskIt/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  TaskIt
+//
+//  Created by Jorge Casariego on 13/3/15.
+//  Copyright (c) 2015 Jorge Casariego. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TaskIt/TaskModel.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  TaskIt
+//
+//  Created by Jorge Casariego on 17/3/15.
+//  Copyright (c) 2015 Jorge Casariego. All rights reserved.
+//
+
+import Foundation
